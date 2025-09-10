14e0d5c00b2a44c50349ad8b96627206d17a7a6d
Carlos Eduardo <78118717+eduardo92005-debug@users.noreply.github.com>
2025-09-09 22:39:45 -0300

    Add files via upload

--- app/database.py
+++ app/database.py
@@ -1,11 +1,43 @@
-import os
-#from sqlalchemy import create_engine, text
+# /app/database.py
+import os, time
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://quiz:R00t1PaS1@db:3306/quizdb")
 
+Base = declarative_base()
+
 engine = None
 last_err = None
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+# tenta conectar ~60s
+for i in range(30):
+    try:
+        tmp = create_engine(
+            DATABASE_URL,
+            pool_pre_ping=True,
+            pool_recycle=3600,
+            future=True,
+        )
+        # testa de VERDADE a conexão
+        with tmp.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        engine = tmp
+        print("✅ Conectado ao MySQL.")
+        break
+    except Exception as e:
+        last_err = e
+        print(f"⏳ MySQL ainda não pronto (tentativa {i+1}/30). Erro: {e}")
+        time.sleep(2)
+
+if engine is None:
+    raise RuntimeError(f"❌ Não consegui conectar ao MySQL: {last_err}")
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/routes/test.py
+++ app/routes/test.py
@@ -4,4 +4,4 @@ router = APIRouter(prefix="/api/test", tags=["Test"])
 
 @router.get("/ping")
 def ping():
-    return {"message": "pong from /test/ping"}
+    return {"message": "meu novo teste vai"}
